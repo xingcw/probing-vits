0aaaf42c132da8aa465279fac3e21d3ab7d1d6c5
chxing <xingcw602@sina.com>
2023-02-27 14:06:04 +0100

    remove require

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 import setuptools
-from pip._internal.req import parse_requirements
-
-
-install_reqs = parse_requirements("requirements.txt", session="test")
-reqs = [str(ir.requirement) for ir in install_reqs]
 
 __version__ = '0.0.1'
 
 setuptools.setup(
     name='prob_vit',
     version=__version__,
-    packages=setuptools.find_packages(),
-    install_requires=reqs
+    packages=setuptools.find_packages()
 )
